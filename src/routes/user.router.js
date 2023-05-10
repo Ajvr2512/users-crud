da40d38cc9a08db12f6d672487dd8c8fea2e2569
Angela Villa <ajvr2512@gmail.com>
2023-05-09 21:36:04 -0500

    Create user.controllers.js

---
+++ src/routes/user.router.js
@@ -0,0 +1,10 @@
+const { getAll, create, getOne, remove, update } = require("../controllers/user.controllers.js");
+const express = require("express");
+
+const userRouter = express.Router();
+
+userRouter.route("/").get(getAll).post(create);
+
+userRouter.route("/:id").get(getOne).delete(remove).put(update)
+
+module.exports = userRouter;
